ffa5e68319f61a147399bde46ca29041855ce60f
yoonsangpark <yoonsangpark@gmail.com>
2025-02-02 17:23:09 +0900

    initial setup

---
+++ koo_resize_room.c
@@ -0,0 +1,23 @@
+/*
+ *
+ * Copyright (C) 2024 KOO Inc.
+ *
+ * Driver for the Koomon
+ *
+ * Licensed under the GPL-2 or later.
+ */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+
+int main(int argc, char** argv)
+{
+	printf("Koo\n");
+
+	return 0;
+}
